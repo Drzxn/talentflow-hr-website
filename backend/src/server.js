ae09961df882c665a0497c4cfa67124f5c3d6703
Drzxn <darshanr1290@gmail.com>
2026-05-11 20:53:47 +0530

    Fix production CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://talent-hr.netlify.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://talent-hr.netlify.app",
+    ],
     credentials: true,
   })
 );
